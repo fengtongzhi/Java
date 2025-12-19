fbbb6ffb27d12b9e66af7c40238ac1239bff12ba
fengtongzhi <2024218630@mail.hfut.edu.cn>
2025-12-19 17:03:46 +0800

    删除 .idea 和 out 文件夹追踪

--- javase/base/src/base01/oop/Test.java
+++
@@ -1,9 +0,0 @@
-package base01.oop;
-
-public class Test {
-    public static void main(String[] args) {
-        Star star= new Star("Tony Leung",58);
-        star.setAge(18);
-        System.out.println(star.getAge());
-    }
-}
